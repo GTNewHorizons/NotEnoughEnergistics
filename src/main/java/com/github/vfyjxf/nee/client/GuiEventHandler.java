9e5a4e4f015b7bd77fbed1c6e6ea319b50a02412
reobf <2215595288@qq.com>
2025-01-15 12:59:38 +0800

    spotless

--- src/main/java/com/github/vfyjxf/nee/client/GuiEventHandler.java
+++ src/main/java/com/github/vfyjxf/nee/client/GuiEventHandler.java
@@ -193,7 +193,8 @@ public class GuiEventHandler implements INEIGuiHandler {
         }
 
         if (NEEConfig.enableNEIDragDrop) {
-            if (gui instanceof AEBaseGui && !gui.getClass().getName().contains("com.glodblock.github.client.gui.GuiLevelMaintainer")) {
+            if (gui instanceof AEBaseGui
+                    && !gui.getClass().getName().contains("com.glodblock.github.client.gui.GuiLevelMaintainer")) {
                 if (draggedStack != null) {
                     Slot currentSlot = gui.getSlotAtPosition(mouseX, mouseY);
                     if (currentSlot instanceof SlotFake) {
